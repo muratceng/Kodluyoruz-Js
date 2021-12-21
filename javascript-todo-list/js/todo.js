515f5c4fe1cc9a9c119d7a91e05235b686cde18f
Murat <muratcengcaliskan@gmail.com>
2021-12-21 20:10:09 +0300

    kod güncellendi

--- javascript-todo-list/js/todo.js
+++ javascript-todo-list/js/todo.js
@@ -5,105 +5,97 @@ const toastSucces = document.querySelector("#liveToast")
 const toastUnSucces = document.querySelector("#liveToastErr")
 let defaultLi = document.getElementsByTagName("li");
 
-addBtn.addEventListener("click", newEelement)
+
+addBtn.addEventListener("click", newElement)
 list.addEventListener("click", deleteElement)
-list.addEventListener("click", missonComplite)
+list.addEventListener("click",checked)
+let items=getStorage();
 
-loadPage()
-
-//Mevcut listede olanlara X butonu ekleme
-for (let i = 0; i < defaultLi.length; i++) {
-    if (defaultLi[i].className == "close") {
+//sayfa içerisinin local storageden gelen bilgilerle dolmasını sağlar.
+for (let i = 0; i < items.length; i++) {
+    if (items[i] == null) {
         console.log("hatalı")
     } else {
-        let span = document.createElement("span")
-        span.classList.add("close")
-        span.innerHTML = "X"
-        defaultLi[i].appendChild(span)
-        console.log("test")
+        let li=document.createElement("li");
+        li.innerHTML=items[i];
+        let span = document.createElement("span");
+        span.classList.add("close");
+        span.innerHTML = "X";
+        li.appendChild(span);
+        list.appendChild(li);
+        console.log("test");
+        
     }
 }
 
-function missonComplite(e) {
-    e.target.classList.toggle("checked")
-
-}
-// yeni inputlar için element etiketlerini(li,span,X)   oluşturma
-function crtElement() {
-    const li = document.createElement("li")
-    const span = document.createElement("span")
-    span.innerHTML = "X"
-    span.classList.add("close")
-    li.innerHTML = `<span>${input.value} </span> `
-    li.appendChild(span)
-    list.appendChild(li)
-
-}
-// girilen imputla yeni element oluşturma
-function newEelement() {
-    if (input.value.trim() != "" && input.value.trim() != " ") {
-        crtElement()
-        loadStorage(input.value)
-        $(".success").toast("show");  // başarılı başarısız mesajları için toast mesajları
-        input.value = ""
-    } else {
-        $(".error").toast("show");
+//list item içerisine item eklemeyi sağlar
+function newElement(){
+    if (input.value.trim()=="" || input.value.trim()==" "){
+    $(".error").toast("show");
+    }  
+    else{
+        let li=document.createElement("li");
+        let span=document.createElement("span")
+        li.innerHTML=input.value;
+        span.innerHTML="X"
+        span.classList.add("close");
+        list.appendChild(li);
+        li.appendChild(span);
+        $(".success").toast("show"); 
+        loadStorage(input.value);
+        input.value=""
     }
 }
-//element silme 
-function deleteElement(e) {
-    if (e.target.className === "close") {
-        if (confirm("Are you Sure ?")) {
-            e.target.parentElement.remove();  // tıklandığı yerdeki "x" parantElementi kaldır(parant element = li)
-            let prm = e.target.parentElement.firstChild.nextElementSibling.textContent.trim();
-            //tıkladıgımız yerin bir üst elementine git(li) first childina gel(span) bu span'ın nextelemnent ile içerisine gir ve textContentini al(metin) ve deletestorage fonksiyonunda çağırdık.
-            deleteStorage(prm);
-        }
 
-    }
-}
-//inputa gerilen veriyi localStorage'ye ekledik 
-function loadStorage(prm) {
-    let storage = JSON.parse(localStorage.getItem("todo"))
-    let toDos;
-    if (storage == null) {
-        toDos = [];
-        console.log("if test ")
-    } else {
-        toDos = getStorage()
-    }
-    toDos.push(prm)
-    localStorage.setItem("todo", JSON.stringify(toDos))
-    console.log("todos  test : " + toDos)
-}
-// loadStorage da var olan localStorage'ı döndürmek için
-function getStorage() {
-    let toDo = JSON.parse(localStorage.getItem("todo"))
-    return toDo
-}
-// Sayfayı yenilediğimizde localStorage verileri listeye ekledik ve  başlangıçta bu fonksiyonu çağırdık
-function loadPage() {
-    let toDo = getStorage()
-    if (toDo != null) {
-        let html
-        for (let i = 0; i < toDo.length; i++) {
-            html = `<li>
-             <span>
-             ${toDo[i]}
-             </span>
-             <span class = "close"> X </span>
-             </li>`
-            list.innerHTML += html
+//list item içerisinden item silmeyi sağlar.
+function deleteElement(item){
+    if(item.target.className=="close"){
+        if(confirm("Are you Sure")){
+            item.target.parentElement.remove();
+            let deleteStr=item.target.parentElement.firstChild.textContent.trim();
+            console.log(deleteStr);
+            deleteStorage(deleteStr);
         }
     }
+    
+    
 }
-// local storage veri silme  ( deleteElement() fonksiyonunda  çağırılıyor)
-function deleteStorage(prm) {
-    let toDo = getStorage()
-    toDo.forEach((element, id) => {
-        if (element == prm) {
-            toDo.splice(id, 1)
+
+//tıklanan list itemin check yapılmasını yada kaldırılmasını sağlar.
+function checked(item){
+    item.target.classList.toggle("checked");
+
+}
+
+//local storageden eleman siler.
+function deleteStorage(value){
+    let values=getStorage();
+    values.forEach((element,id) => {
+        if(element==value){
+            values.splice(id,1);
         }
+        
     });
-    localStorage.setItem("todo", JSON.stringify(toDo))
+    localStorage.setItem("todo",JSON.stringify(values));
+}
+
+//local storage içerisine yeni eleman ekler.
+function loadStorage(value){
+    let values=getStorage()
+    let temp;
+    if(values==null){
+        temp=[];
+    }
+    else{
+        temp=[...values];
+    }
+    temp.push(value);
+    localStorage.setItem("todo",JSON.stringify(temp));
+
+}
+
+//localstoragedeki değerleri döndürür.
+function getStorage(){
+    let values=JSON.parse(localStorage.getItem("todo"));
+    return values;
 }
